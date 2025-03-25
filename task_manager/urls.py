6859753db6229ad8dac72ac7d4c0931bce55bc65
Akiwork1252 <aki1252work@gmail.com>
2025-03-25 15:40:37 +0900

    Create:interest category display, add function and delete function

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -7,4 +7,7 @@ app_name = 'task_manager'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('inquiry/', views.InquiryView.as_view(), name='inquiry'),
+    path('interest_category_list/', views.InterestCategoryListView.as_view(), name='interest_category'),
+    path('add_interest_category/', views.AddInterestCategoryView.as_view(), name='add_interest_category'),
+    path('delete_interest_category/<int:category_id>/', views.DeleteInterestCategory.as_view(), name='delete_interest_category'),
 ]
